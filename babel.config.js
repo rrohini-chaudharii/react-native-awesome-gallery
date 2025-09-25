7880aea3ff17d99593daf80bbea5247d15c26f30
rrohini-chaudharii <159746575+rrohini-chaudharii@users.noreply.github.com>
2025-09-25 21:12:11 +0530

    Refactor babel.config.js to use function syntax

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,14 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      "module:metro-react-native-babel-preset",
+      "@babel/preset-typescript"
+    ],
+    plugins: [
+      // any extra plugins you require, e.g. optional chaining, nullish
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-proposal-nullish-coalescing-operator"
+    ]
+  };
 };
